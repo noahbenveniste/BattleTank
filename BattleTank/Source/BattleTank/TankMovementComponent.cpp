24ba7020be8ebcd5458d15f6b9897ecc7c16b531
Noah Benveniste <benven.noa1995@gmail.com>
2019-06-20 12:36:46 -0400

    Issue #11: Committing files for TankMovementComponent that should have been in last commit.

---
+++ BattleTank/Source/BattleTank/TankMovementComponent.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankMovementComponent.h"
+#include "TankTrack.h"
+
+void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Error: One or more of the TankTrack pointers is null"));
+		return;
+	}
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
+
+void UTankMovementComponent::IntendMoveForward(float Throw)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+}
+
+
